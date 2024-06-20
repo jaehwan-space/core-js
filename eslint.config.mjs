712243040dd09cdf54f85ef765e2141a96c182f7
jaehwan-space <ma2582m@naver.com>
2024-06-20 09:20:41 +0900

    [6월 19일] 학습

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
+const ignore = {
+  isString: true,
+  isObject: true,
+  isArray: true,
+  isNull: true,
+  getNode: true,
+  insertLast: true,
+  attr: true,
+  css: true,
+  bindEvent: true,
+  addClass: true,
+  removeClass: true,
+};
 
 export default [
   pluginJs.configs.recommended,
   {
-    languageOptions: { 
+    languageOptions: {
       globals: {
-        ...globals.browser, 
-        ...globals.node
-      } 
+        ...globals.browser,
+        ...globals.node,
+        ...ignore,
+      },
     },
+
     rules: {
-      "no-unused-vars": "warn",
+      'no-unused-vars': 'warn',
     },
   },
 ];
