49c6f5fadeec6ea2e93ac94159de6bd7a2df5ba5
jaehwan-space <ma2582m@naver.com>
2024-06-20 10:44:45 +0900

    [6월 20일] 스타벅스 헤더 GSAP 애니메이션

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -1,19 +1,16 @@
 const aList = document.querySelectorAll('nav a');
-const depthList = document.querySelectorAll('.depth');
-const header = document.querySelector('#header');
 
-const h = (t) => (t.style.height = '0');
+/* global gsap */
 
 aList.forEach((a) => {
+  const target = a.lastElementChild;
+
+  const tl = gsap.timeline({ paused: true }).to(target, { height: 100 });
+
   a.addEventListener('mouseenter', () => {
-    const target = a.lastElementChild;
-
-    depthList.forEach(h);
-
-    target.style.height = '100px';
+    tl.play();
   });
-});
-
-header.addEventListener('mouseleave', () => {
-  depthList.forEach(h);
+  a.addEventListener('mouseleave', () => {
+    tl.reverse();
+  });
 });
